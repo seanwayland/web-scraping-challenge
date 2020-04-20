4a17bf0ac506e3cd546b4b2c4028076893c77997
sean wayland <seanwayland@gmail.com>
2020-04-20 14:23:42 -0400

    imageurl

--- mission_to_mars.py
+++ mission_to_mars.py
@@ -1,18 +1,21 @@
 import time
 from splinter import Browser
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 import pandas as pd
 import requests
 #executable path
+
+
 executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 # HTML object
 url = 'https://mars.nasa.gov/news/?'
 browser.visit(url)
 html = browser.html
-time.sleep( 5 )
+time.sleep(5)
 # Parse HTML with Beautiful Soup
-
+'''
 soup = BeautifulSoup(html, 'html.parser')
 # Parse HTML with Beautiful Soup
 
@@ -23,27 +26,38 @@ latest_teaser = soup.find('div', class_='list_text').find('div', class_='article
 print(latest_teaser)
 
 
+'''
 
 
+
+
+
+url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(url2)
+time.sleep(5)
+browser.click_link_by_partial_text('FULL IMAGE')
+html = browser.html
+time.sleep(5)
+# Parse HTML with Beautiful Soup
+
+soup = BeautifulSoup(html, 'html.parser')
+a = soup.find_all('a', class_= 'fancybox')
+images = []
+for val in a:
+    images.append(val.get('data-fancybox-href'))
+
+imgURL = images[1]
+baseURL = 'https://www.jpl.nasa.gov'
+fullURL = baseURL + imgURL
+
+print(fullURL)
 
 '''
-results = []
-filtered = []
-for x in  soup.find_all( class_='list_text'):
-    results.append(x.text)
-for y in results:
-    print(y)
+imgURL = a.get('data-fancybox-href')
+baseURL = 'https://www.jpl.nasa.gov'
+fullURL = baseURL+ imgURL
+print(fullURL)
+'''
 
-'''
-'''
-results = soup.find_all('div', class_="slide")
-for result in results:
-    titles = result.find('div', class_="content_title")
-    title = titles.find('a').text
-    bodies = result.find('div', class_="rollover_description")
-    body = bodies.find('div', class_="rollover_description_inner").text
-    print('----------------')
-    print(title)
-    print(body)
 
-'''
+
